5b9077a82f46edcde5c7749cd0a44a76d4347ee0
Amr Hassan <amrtechguy@gmail.com>
2022-03-14 23:59:20 +0200

    Add Account.cpp
    Added the function implementations

---
+++ src/Account.cpp
@@ -0,0 +1,35 @@
+#include "../include/Account.h"
+
+Account::Account()
+    : id {0}, username {""}, password {""}, logged_in {false}
+{}
+
+Account::Account(std::string username, std::string password)
+    : id {0}, username {""}, password {""}, logged_in {false}
+{
+    login(username, password);
+}
+
+void Account::login(std::string username, std::string password)
+{
+    // Hard-coded authentication, until using a database
+    if(username == "admin" && password == "1234")
+    {
+        logged_in = true;
+    }
+}
+
+bool Account::is_logged_in()
+{
+    return logged_in;
+}
+
+std::string Account::get_username()
+{
+    return username;
+}
+
+void Account::logout()
+{
+    logged_in = false;
+}
