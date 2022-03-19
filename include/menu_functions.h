ae414faf58b811ca930c4a3bf491d1d2b6354156
Amr Hassan <amrtechguy@gmail.com>
2022-03-19 21:56:21 +0200

    Update menu_functions.h
    Added function declarations for menu options

--- include/menu_functions.h
+++ include/menu_functions.h
@@ -1,6 +1,124 @@
 #ifndef MENU_FUNCTIONS
 #define MENU_FUNCTIONS
 
+void display_doctors()
+{
 
+}
+
+void display_doctor()
+{
+    
+}
+
+void add_doctor()
+{
+    
+}
+
+void edit_doctor()
+{
+    
+}
+
+void delete_doctor()
+{
+    
+}
+
+void display_patients()
+{
+
+}
+
+void display_patient()
+{
+    
+}
+
+void add_patient()
+{
+    
+}
+
+void edit_patient()
+{
+    
+}
+
+void delete_patient()
+{
+    
+}
+
+void display_services()
+{
+
+}
+
+void display_service()
+{
+    
+}
+
+void add_service()
+{
+    
+}
+
+void edit_service()
+{
+    
+}
+
+void delete_service()
+{
+    
+}
+
+void display_appointments()
+{
+
+}
+
+void display_appointment()
+{
+    
+}
+
+void add_appointment()
+{
+    
+}
+
+void edit_appointment()
+{
+    
+}
+
+void delete_appointment()
+{
+    
+}
+
+void edit_account_username()
+{
+    
+}
+
+void edit_account_password()
+{
+    
+}
+
+void edit_system_name()
+{
+    
+}
+
+void edit_system_address()
+{
+    
+}
 
 #endif
