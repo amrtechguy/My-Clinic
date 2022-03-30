7cd8355f2f028c587e7f5b51e0f587d12a6f7f91
Amr Hassan <amrtechguy@gmail.com>
2022-03-30 17:20:31 +0200

    Update DB.h

--- include/DB.h
+++ include/DB.h
@@ -1,6 +1,7 @@
 #ifndef DB_H
 #define DB_H
 
+#include <typeinfo>
 #include <string>
 #include <vector>
 #include <sqlite3.h>
@@ -9,14 +10,54 @@ class DB
 {
 private:
 	std::string db_file;
-	sqlite3 *db_handle {nullptr};
-	std::vector<std::vector<std::string>> data;
+	sqlite3 *db_handle;
+	std::string sql_query;
+	void *data;
 
 private:
-	static void connect();
+	bool connect();
+	std::string get_query_key();
+	friend int store_selected_data(void* data, int argc, char** argv, char** azColName);
 
 public:
-	static void query(std::string sql);
+	DB();
+	void query(std::string sql);
+	auto get_data()
+	{
+		return (static_cast<std::vector<std::vector<std::string>>*>(data));
+	}
+	void reset();
+	~DB();
 };
+
+static int store_selected_data(void* data, int argc, char** argv, char** azColName)
+{
+	std::vector<std::string> single_row;
+
+	// store the column names
+	if ((static_cast<std::vector<std::vector<std::string>>*>(data))->size() == 0)
+	{
+		for (int i{0}; i < argc; i++)
+		{
+			single_row.push_back(azColName[i]);
+		}
+
+		(static_cast<std::vector<std::vector<std::string>>*>(data))->push_back(single_row);
+		single_row.erase(single_row.begin(), single_row.end());
+	}
+	else
+	{
+		// store the rows
+		for (int i{0}; i < argc; i++)
+		{
+			single_row.push_back(argv[i]);
+		}
+
+		(static_cast<std::vector<std::vector<std::string>>*>(data))->push_back(single_row);
+		single_row.erase(single_row.begin(), single_row.end());
+	}
+
+	return 0;
+}
 
 #endif
